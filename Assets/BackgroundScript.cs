8cf7c95aa1bd18e91f02296aa9fac30662b9e959
viniciuspreis <73563782+viniciuspreis@users.noreply.github.com>
2021-03-27 15:57:23 -0300

    Initial commit

---
+++ Assets/BackgroundScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    private float screenWidth;
+
+    void Start()
+    {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        float imageWidth = renderer.sprite.bounds.size.x;
+        float imageHeight = renderer.sprite.bounds.size.y;
+        
+        float screenHeight = Camera.main.orthographicSize * 2;
+
+        screenWidth = screenHeight /  Screen.height * Screen.width;
+
+        Vector2 newScale = this.transform.localScale;
+
+        newScale.x = screenWidth / imageWidth + 0.25f;
+        newScale.y = screenHeight / imageHeight;
+
+        this.transform.localScale = newScale;
+
+        body.velocity = new Vector2(-3, 0);
+
+        if (this.name == "Background2")
+        {
+            this.transform.position = new Vector2(screenWidth, 0);
+        }
+    }
+
+    void Update()
+    {
+        if (this.transform.position.x <= -screenWidth)
+        {
+            this.transform.position = new Vector2(screenWidth, 0);
+        }
+    }
+}
---
+++ Assets/HeroiScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroiScript : MonoBehaviour
+{
+    public float jumpForce;
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        this.body = GetComponent<Rigidbody2D>();
+        this.body.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            this.body.isKinematic = false;
+            this.body.AddForce(new Vector2(0, this.jumpForce));
+        }
+
+        this.transform.rotation = Quaternion.Euler(0, 0, body.velocity.y * 3);
+    }
+}
